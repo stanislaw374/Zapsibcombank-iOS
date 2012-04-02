5f30ceb0574f4d4f6d667c2a93cbeae064d0f43e
Stanislaw Yazhenskikh <stanislaw.yazhenskikh@gmail.com>
2012-04-02 11:04:01 +0600

    Initial Commit

---
+++ Zabsibcombank/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Zabsibcombank
+//
+//  Created by Yazhenskikh Stanislaw on 02.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ZabsibcombankTests/ZabsibcombankTests.h
@@ -0,0 +1,13 @@
+//
+//  ZabsibcombankTests.h
+//  ZabsibcombankTests
+//
+//  Created by Yazhenskikh Stanislaw on 02.04.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ZabsibcombankTests : SenTestCase
+
+@end

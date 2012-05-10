aabd0b971fc32e1aa071b16c559a9a7fc8dcf7d7
Kirill <kirillkunst@gmail.com>
2012-05-10 14:55:10 +0600

    Анимация Депозиты

--- Code/Controllers/DepositsViewController.h
+++ Code/Controllers/DepositsViewController.h
@@ -9,5 +9,28 @@
 #import <UIKit/UIKit.h>
 
 @interface DepositsViewController : UIViewController
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *imgRed;
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *imgViolet;
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *imgGreen;
+@property (unsafe_unretained, nonatomic) IBOutlet UITextView *txtRed;
+@property (unsafe_unretained, nonatomic) IBOutlet UITextView *txtViolet;
+@property (unsafe_unretained, nonatomic) IBOutlet UITextView *txtGreen;
 
+@property (unsafe_unretained, nonatomic) IBOutlet UIButton *bluePlus;
+
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *blueGear1;
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *blueGear2;
+@property (unsafe_unretained, nonatomic) IBOutlet UIImageView *blueGear3;
+
+- (IBAction)bluePlusClick:(id)sender;
+
+-(void)animateFirstArrows;
+-(void)animateSecondArrows;
+-(void)animateThirdArrows;
+-(void)animateFoursArrows;
+
+-(void)animateFirstGears;
+-(void)animateSecondGears;
+-(void)animateThirdGears;
+-(void)animateFoursGears;
 @end
--- Code/Controllers/DepositsViewController.m
+++ Code/Controllers/DepositsViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "DepositsViewController.h"
-
+#import <QuartzCore/QuartzCore.h> 
 @interface DepositsViewController ()
 @property (unsafe_unretained, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (unsafe_unretained, nonatomic) IBOutlet UILabel *lblOvernightDeposit;
@@ -22,6 +22,16 @@
 @end
 
 @implementation DepositsViewController
+@synthesize imgRed;
+@synthesize imgViolet;
+@synthesize imgGreen;
+@synthesize txtRed;
+@synthesize txtViolet;
+@synthesize txtGreen;
+@synthesize bluePlus;
+@synthesize blueGear1;
+@synthesize blueGear2;
+@synthesize blueGear3;
 @synthesize scrollView;
 @synthesize lblOvernightDeposit;
 @synthesize lblStandardDeposit;
@@ -40,10 +50,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    [self animateFirstArrows];
     // Do any additional setup after loading the view from its nib.    
     self.scrollView.contentSize = CGSizeMake(1024, 4854);
 }
+-(void) viewDidAppear:(BOOL)animated{
+    [self animateFirstArrows];
+}
 
+-(void)animateFirstArrows{
+    [UIView animateWithDuration:0.5f delay:0.25f options:UIViewAnimationOptionRepeat | UIViewAnimationOptionAutoreverse | UIViewAnimationOptionAllowUserInteraction  animations:^{
+        self.bluePlus.transform = CGAffineTransformMakeScale(1.3, 1.3);
+    }completion:nil];
+
+}
 - (void)viewDidUnload
 {
     [self setLblOvernightDeposit:nil];
@@ -51,6 +71,16 @@
     [self setLblConstructorDeposit:nil];
     [self setLblPartnerDeposit:nil];
     [self setScrollView:nil];
+    [self setImgRed:nil];
+    [self setImgViolet:nil];
+    [self setImgGreen:nil];
+    [self setTxtRed:nil];
+    [self setTxtViolet:nil];
+    [self setTxtGreen:nil];
+    [self setBlueGear1:nil];
+    [self setBlueGear2:nil];
+    [self setBlueGear3:nil];
+    [self setBluePlus:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -74,4 +104,48 @@
 - (IBAction)showPartnerDeposit {
 }
 
+- (IBAction)bluePlusClick:(id)sender {
+    if ([sender tag] == 1){
+        [sender setTag:3];
+        [[self.bluePlus layer] removeAllAnimations];
+        self.bluePlus.transform = CGAffineTransformMakeScale(1, 1);
+        [UIView animateWithDuration:1.0f
+                         animations:^{
+                             CGRect frame = imgRed.frame;
+                             frame.origin.x += 540;
+                             imgRed.frame = frame;
+                             
+                             frame = txtRed.frame;
+                             frame.origin.x -= 458;
+                             txtRed.frame = frame; 
+                             
+                         }completion:^(BOOL finished){
+                             [UIView animateWithDuration:1.0f
+                                              animations:^{
+                                                  CGRect frame = imgViolet.frame;
+                                                  frame.origin.x += 558;
+                                                  imgViolet.frame = frame;
+                                                  
+                                                  frame = txtViolet.frame;
+                                                  frame.origin.x -= 458;
+                                                  txtViolet.frame = frame;                                              
+                                              
+                                              }completion:^(BOOL finished){
+                                                  [UIView animateWithDuration:1.0f
+                                                                   animations:^{
+                                                                       CGRect frame = imgGreen.frame;
+                                                                       frame.origin.x += 540;
+                                                                       imgGreen.frame = frame;
+                                                                       
+                                                                       frame = txtGreen.frame;
+                                                                       frame.origin.x -= 458;
+                                                                       txtGreen.frame = frame;                                              
+                                                                       
+                                                                   }completion:nil];
+                                              }];
+                         }];
+        
+    }
+
+}
 @end

2416a777610704484ac74813ff8e080f7cc5dd5d
Stanislav Yazhenskikh <stanislaw.yazhenskikh@gmail.com>
2012-05-11 12:09:19 +0600

    Экран "Брокерные сервисы" с анимацией

---
+++ Code/Controllers/BrokerServiceViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrokerServiceViewController.h
+//  Zapsibcombank
+//
+//  Created by Yazhenskikh Stanislaw on 11.05.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrokerServiceViewController : UIViewController
+
+@end
